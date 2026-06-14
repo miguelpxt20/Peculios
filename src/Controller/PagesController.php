2ad5dfc463371a2cfb234aae232f8f155e08e247
miguelpxt <miguelpxt2003@gmail.com>
2026-06-14 13:20:39 -0300

    dashboard com totais e acesso rápido

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -1,19 +1,6 @@
 <?php
 declare(strict_types=1);
 
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link      https://cakephp.org CakePHP(tm) Project
- * @since     0.2.9
- * @license   https://opensource.org/licenses/mit-license.php MIT License
- */
 namespace App\Controller;
 
 use Cake\Core\Configure;
@@ -22,27 +9,8 @@ use Cake\Http\Exception\NotFoundException;
 use Cake\Http\Response;
 use Cake\View\Exception\MissingTemplateException;
 
-/**
- * Static content controller
- *
- * This controller will render views from templates/Pages/
- *
- * @link https://book.cakephp.org/4/en/controllers/pages-controller.html
- */
 class PagesController extends AppController
 {
-    /**
-     * Displays a view
-     *
-     * @param string ...$path Path segments.
-     * @return \Cake\Http\Response|null
-     * @throws \Cake\Http\Exception\ForbiddenException When a directory traversal attempt.
-     * @throws \Cake\View\Exception\MissingTemplateException When the view file could not
-     *   be found and in debug mode.
-     * @throws \Cake\Http\Exception\NotFoundException When the view file could not
-     *   be found and not in debug mode.
-     * @throws \Cake\View\Exception\MissingTemplateException In debug mode.
-     */
     public function display(string ...$path): ?Response
     {
         if (!$path) {
@@ -51,8 +19,12 @@ class PagesController extends AppController
         if (in_array('..', $path, true) || in_array('.', $path, true)) {
             throw new ForbiddenException();
         }
-        $page = $subpage = null;
 
+        if ($path[0] === 'home') {
+            return $this->dashboard();
+        }
+
+        $page = $subpage = null;
         if (!empty($path[0])) {
             $page = $path[0];
         }
@@ -69,5 +41,42 @@ class PagesController extends AppController
             }
             throw new NotFoundException();
         }
+    }
+
+    public function dashboard(): ?Response
+    {
+        $associadosTable = $this->fetchTable('Associados');
+        $contratosTable = $this->fetchTable('ContratosPeculio');
+        $sinistrosTable = $this->fetchTable('Sinistros');
+        $contribuicoesTable = $this->fetchTable('Contribuicoes');
+
+        // Totais para o dashboard
+        $totalAssociadosAtivos = $associadosTable->find()
+            ->where(['situacao' => 'ativo'])
+            ->count();
+
+        $totalContratosVigentes = $contratosTable->find()
+            ->where(['status' => 'vigente'])
+            ->count();
+
+        $totalSinistrosAbertos = $sinistrosTable->find()
+            ->where(['status IN' => ['aberto', 'em_analise']])
+            ->count();
+
+        $totalContribuicoesPendentes = $contribuicoesTable->find()
+            ->where([
+                'status' => 'pendente',
+                'competencia' => date('Y-m-01'),
+            ])
+            ->count();
+
+        $this->set(compact(
+            'totalAssociadosAtivos',
+            'totalContratosVigentes',
+            'totalSinistrosAbertos',
+            'totalContribuicoesPendentes'
+        ));
+
+        return $this->render('home');
     }
 }
